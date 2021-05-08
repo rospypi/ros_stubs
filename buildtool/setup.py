5c42a3b74ac0e44cff74cf72588d22d1462359d6
Yuki Igarashi <me@bonprosoft.com>
2021-05-09 00:13:22 +0900

    Install genmypy from vcs

--- buildtool/setup.py
+++ buildtool/setup.py
@@ -10,7 +10,7 @@ setup(
     install_requires=[
         "PyYAML>=5.4,<6.0",
         "build>=0.3.0,<0.4.0",
-        "genpyi>=0.1.0,<0.2.0",
+        "genmypy>=0.3.0,<0.4.0",
         "pydantic>=1.8.0,<2.0.0",
     ],
     package_data={"buildtool": ["py.typed"]},
